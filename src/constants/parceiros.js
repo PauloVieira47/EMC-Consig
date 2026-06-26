76e10a77d4bf34dc60309886d1eb0a05357244ed
PauloVieira47 <paulovieiranunes3@gmail.com>
2026-06-26 13:47:15 -0300

    adiçao da logo do brb

--- src/constants/parceiros.js
+++ src/constants/parceiros.js
@@ -3,6 +3,6 @@ export const bancosParceiros = [
   { src: "/img/bancos/logo-c6-bank.png", alt: "C6 Bank", lightOnDark: true },
   { src: "/img/bancos/bancoDobrasil.png", alt: "Banco Do brasil" },
   { src: "/img/bancos/caixa-economica.png", alt: "Caixa Economica" },
-  { src: "/img/bancos/BRB_logo.png", alt: "BRB" },
+  { src: "/img/bancos/BRB_Logo.png", alt: "BRB" },
   { src: "/img/bancos/Logo-banco-inter.png", alt: "Banco Inter" },
 ];
